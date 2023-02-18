e3607f48318a6bce1b06f9da2a68c9c12b757b8c
Lily <lilyyma16@gmail.com>
2023-02-18 04:00:14 -0800

    starting to add routing

--- backend/store.js
+++ backend/store.js
@@ -1,3 +1,6 @@
+// gets text, etc from frontend & just stores it in the Mongo database
+// also parses text for main ideas (with gpt) if the main idea set is None in database, then stores set in db as well
+
 const express = require('express')
 const { Configuration, OpenAIApi } = require("openai");
 const bodyParser = require('body-parser');
@@ -10,12 +13,10 @@ const configuration = new Configuration({apiKey: token});
 const openai = new OpenAIApi(configuration);
 
 const app = express();
-const things = require('./things.js')
-app.use('/message', things) // router???? wtf is this
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/message', (req, res) => {
+app.post('/store', (req, res) => {
     const response = openai.createCompletion({
         model: 'text-davinci-003',
         prompt: req.body.prompt
@@ -29,13 +30,3 @@ app.post('/message', (req, res) => {
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
-
-// async function runCompletion () {
-//     const completion = await openai.createCompletion({
-//     model: "text-davinci-003",
-//     prompt: "How are you today?",
-//     });
-//     console.log(completion.data.choices[0].text);
-// }
-
-// runCompletion();
